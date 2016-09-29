efd487cacb48b52b711464e7874139dcce6ddfe9
Ethan <yishionwang314@163.com>
2016-09-29 17:40:44 +0800

    FEAT: GROUND COLLIDER

---
+++ Assets/_Scripts/Ground.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour
+{
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.name == "ingame_0(Clone)") {
+			Destroy (other.gameObject);
+		}
+	}
+}
